a574171c74928946cac43544f9f7fdb92db09c2b
bopo <ibopo@126.com>
2023-05-07 16:01:01 +0800

    test(test): 更新测试代码

--- tests/test_guaci.py
+++ tests/test_guaci.py
@@ -1,14 +1,5 @@
-'''
-# 六亲
-QING6 = ("兄弟", "父母", "官鬼", "妻财", "子孙")
-
-# 五行
-XING5 = ('木', '火', '土', '金', '水')
-'''
-import pickle
+from najia.utils import get_guaci
 
 
 def test_guaci():
-    gc = pickle.load(open('najia/data/gc.pkl', 'rb'))
-
-    print(gc.get('乾为天'))
+    assert (get_guaci('乾为天'))
