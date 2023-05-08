3c7d12a6aaca95838b376b5cabd014a1f57cb1a2
bopo <ibopo@126.com>
2023-05-08 10:40:21 +0800

    fix (update) update tests.

--- tests/test_compile.py
+++ tests/test_compile.py
@@ -1,6 +1,12 @@
+from najia import Najia
 from najia.utils import xkong
 
 
 def test_compile():
+    result = Najia().compile(params=[2, 2, 1, 2, 0, 2], date='2019-12-25 00:20').render()
+    assert result
+
+
+def test_xkong():
     assert xkong('甲子') == '戌亥'
     assert xkong([0, 0]) == '戌亥'
